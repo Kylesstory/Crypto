4cd863b3b40dc99f5658fe02a240297a5a7d4cbf
Kyle <foxleooo@gmail.com>
2019-10-14 00:45:28 +0800

    minor modification

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from Signature import DigitalSignature as DS
 from Commitment import Commitment as Commit
 
 message = 79
-# pkes = [PKE.RSA(256), PKE.ElGamal(128), PKE.Paillier(128), PKE.CramerShoup(128)]
-# dss = [DS.RSA(256), DS.DSA(128)]
+pkes = [PKE.RSA(256), PKE.ElGamal(128), PKE.Paillier(128), PKE.CramerShoup(128)]
+dss = [DS.RSA(256), DS.DSA(128)]
 commits = [Commit.HashCommit(256), Commit.ElGamal(128), Commit.Pederson(128)]
 
-# for pke in pkes:
-# 	pke.demo(message)
+for pke in pkes:
+	pke.demo(message)
 
-# for ds in dss:
-# 	ds.demo(message)
+for ds in dss:
+	ds.demo(message)
 
 for commit in commits:
 	commit.demo(message)
